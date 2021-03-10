7fcd77a418d1755e0009c3edf4e3a9d9adbf91cb
Lucas Bastos <lucas.b@adireto.com>
2021-03-10 15:24:28 -0300

    feat: add room component

--- src/Components/Wall/Wall.interface.ts
+++ src/Components/Wall/Wall.interface.ts
@@ -1,4 +1,5 @@
 export interface IWall {
+  number: number;
   height: number;
   width: number;
   windows: number;
--- src/Components/Wall/Wall.tsx
+++ src/Components/Wall/Wall.tsx
@@ -1,58 +1,60 @@
-import React from 'react'
-import './Wall.css'
+import React, { useState } from 'react'
 import { IWall } from './Wall.interface'
 
-
-export default function Wall() {
-  const wall: IWall = {
-    height: 0,
-    width: 0,
-    doors: 0,
-    windows: 0
-   }
-   const walls: IWall[] = [wall, wall, wall, wall]
+const Wall = ({wall}: { wall: IWall }) => {
+  const [height, setHeight] = useState('')
+  const [width, setWidth] = useState('')
+  const [windows, setWindows] = useState('')
+  const [doors, setDoors] = useState('')
 
   return (
-    <div>
-      <h2 className="bg-1">Parede 1</h2>
-      <form>
-        <label>
-          <i className="fas fa-arrows-alt-v color-1"></i>
-          <input
-            name="altura"
-            max="15"
-            min="1"
-            type="number"
-            placeholder="Altura em metros"
-          />
-        </label>
-        <label>
-          <i className="fas fa-arrows-alt-h color-1"></i>
-          <input
-            name="largura"
-            max="15"
-            min="1"
-            type="number"
-            placeholder="Largura em metros"
-          />
-        </label>
-        <label>
-          <i className="fas fa-door-closed color-1"></i>
-          <input
-            name="portas"
-            type="number"
-            placeholder="Nº de portas"
-          />
-        </label>
-        <label>
-          <i className="fas fa-window-frame color-1"></i>
-          <input 
-          name="janelas"
-          type="number"      
-          placeholder="Nº de janelas"
-          />
-        </label>
-      </form>
-    </div>
+    <>
+      <label>
+        <i className={`fas fa-arrows-alt-v color-${wall.number}`}></i>
+        <input
+          name="altura"
+          max="15"
+          min="1"
+          type="number"
+          placeholder="Altura em metros"
+          value={height}
+          onChange={(e) => {setHeight(e.target.value)}}
+        />
+      </label>
+      <label>
+        <i className={`fas fa-arrows-alt-h color-${wall.number}`}></i>
+        <input
+          name="largura"
+          max="15"
+          min="1"
+          type="number"
+          placeholder="Largura em metros"
+          value={width}
+          onChange={(e) => {setWidth(e.target.value)}}
+        />
+      </label>
+      <label>
+        <i className={`fas fa-door-closed color-${wall.number}`}></i>
+        <input
+          name="portas"
+          type="number"
+          placeholder="Nº de portas"
+          value={doors}
+          onChange={(e) => {setDoors(e.target.value)}}
+        />
+      </label>
+      <label>
+        <i className={`fas fa-window-frame color-${wall.number}`}></i>
+        <input 
+        name="janelas"
+        type="number"      
+        placeholder="Nº de janelas"
+        value={windows}
+        onChange={(e) => {setWindows(e.target.value)}}
+        />
+      </label>
+    </>
   )
 }
+
+export default Wall
